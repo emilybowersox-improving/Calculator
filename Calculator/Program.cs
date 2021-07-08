8d2d6e3c8c47c0e47eb4e05e8751ab069fd3c9e3
Emily Bowersox <emily.bowersox@improving.com>
2021-07-07 23:26:30 -0500

    working on 'checking inputs'

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,20 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Calculator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int number1;
+            int number2;
+
             Console.WriteLine("A Console Calculator");
             Console.WriteLine("Enter a number.");
-            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                number1 = Convert.ToInt32(userInput);
+
+            } catch
+            {
+                Console.WriteLine(userInput + " is not a valid number.");
+            }
+
             Console.WriteLine("Enter a second number, and I will add it to the first.");
-            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            string userInput2 = Console.ReadLine();
+
+            try
+            {
+                number2 = Convert.ToInt32(userInput2);
+            }
+            catch
+            {
+                Console.WriteLine(userInput2 + " is not a valid number.");
+            }
+
             int result = number1 + number2;
             Console.WriteLine("Result: " + result);
             Console.ReadLine();
+
+            /*            if (userInput.GetType())
+                        if (Console.ReadLine().GetType == int)*/
+
+
+
+            /*            decimal number1 = Convert.ToDecimal(Console.ReadLine());
+                        int number1 = Convert.ToInt32(Console.ReadLine());
+                        // implicit conversion from int to decimal 
+                        int i = 345;
+                        decimal f = i;*/
 
 
         }
