b440713ff282fc1cd1b2b3260d64a8a43d61e8cd
Emily Bowersox <emily.bowersox@improving.com>
2021-07-07 20:24:27 -0500

    basic addition

---
+++ Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A Console Calculator");
+            Console.WriteLine("Enter a number.");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number, and I will add it to the first.");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int result = number1 + number2;
+            Console.WriteLine("Result: " + result);
+            Console.ReadLine();
+
+
+        }
+    }
+}
