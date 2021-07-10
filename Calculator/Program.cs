df04cfbfda19f4dd90e6d9dd3895ef01cff9d912
Emily Bowersox <emily.bowersox@improving.com>
2021-07-10 13:23:19 -0500

    added do-while loop to keep asking the user for a number until they enter a valid one

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,50 +11,47 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            int number1;
-            int number2;
+            decimal number1 = 0;
+            decimal number2 = 0;
+            string userInput1 = "";
+            string userInput2 = "";
 
             Console.WriteLine("A Console Calculator");
-            Console.WriteLine("Enter a number.");
 
-            string userInput = Console.ReadLine();
-
-            try
+          
+            do
             {
-                number1 = Convert.ToInt32(userInput);
+                Console.WriteLine("Enter a number.");
+                userInput1 = Console.ReadLine();
+                if (decimal.TryParse(userInput1, out number1) == true)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(userInput1 + " is not a valid number.");
+                }
+            } while (decimal.TryParse(userInput1, out number1) == false);
 
-            } catch
+
+            do
             {
-                Console.WriteLine(userInput + " is not a valid number.");
-            }
+                Console.WriteLine("Enter a second number, and I will add it to the first.");
+                userInput2 = Console.ReadLine();
+                if (decimal.TryParse(userInput2, out number2) == true)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(userInput2 + " is not a valid number.");
+                }
+            } while (decimal.TryParse(userInput2, out number2) == false);
 
-            Console.WriteLine("Enter a second number, and I will add it to the first.");
 
-            string userInput2 = Console.ReadLine();
-
-            try
-            {
-                number2 = Convert.ToInt32(userInput2);
-            }
-            catch
-            {
-                Console.WriteLine(userInput2 + " is not a valid number.");
-            }
-
-            int result = number1 + number2;
+            decimal result = number1 + number2;
             Console.WriteLine("Result: " + result);
             Console.ReadLine();
-
-            /*            if (userInput.GetType())
-                        if (Console.ReadLine().GetType == int)*/
-
-
-
-            /*            decimal number1 = Convert.ToDecimal(Console.ReadLine());
-                        int number1 = Convert.ToInt32(Console.ReadLine());
-                        // implicit conversion from int to decimal 
-                        int i = 345;
-                        decimal f = i;*/
 
 
         }
